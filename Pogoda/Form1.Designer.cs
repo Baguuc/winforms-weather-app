aad622347c75cb0755fb4318f2f9e4164047327e
Jan Tadeusz Żukowski <4886712@teb-gdansk.ad>
2024-10-02 10:19:02 +0200

    update: improved ui

--- Pogoda/Form1.Designer.cs
+++ Pogoda/Form1.Designer.cs
@@ -29,21 +29,50 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
+            WeatherInfoGrid = new TableLayoutPanel();
+            timer1 = new System.Windows.Forms.Timer(components);
             TemperatureValueLabel = new Label();
             HumidityValueLabel = new Label();
             RainValueLabel = new Label();
-            tableLayoutPanel1 = new TableLayoutPanel();
-            WeatherTimer = new System.Windows.Forms.Timer(components);
-            tableLayoutPanel1.SuspendLayout();
+            TemperatureLabel = new Label();
+            HumidityLabel = new Label();
+            RainLabel = new Label();
+            WeatherInfoGrid.SuspendLayout();
             SuspendLayout();
+            // 
+            // WeatherInfoGrid
+            // 
+            WeatherInfoGrid.ColumnCount = 3;
+            WeatherInfoGrid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            WeatherInfoGrid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333359F));
+            WeatherInfoGrid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333359F));
+            WeatherInfoGrid.Controls.Add(TemperatureValueLabel, 0, 1);
+            WeatherInfoGrid.Controls.Add(HumidityValueLabel, 1, 1);
+            WeatherInfoGrid.Controls.Add(RainValueLabel, 2, 1);
+            WeatherInfoGrid.Controls.Add(TemperatureLabel, 0, 0);
+            WeatherInfoGrid.Controls.Add(HumidityLabel, 1, 0);
+            WeatherInfoGrid.Controls.Add(RainLabel, 2, 0);
+            WeatherInfoGrid.Location = new Point(175, 149);
+            WeatherInfoGrid.Name = "WeatherInfoGrid";
+            WeatherInfoGrid.RowCount = 2;
+            WeatherInfoGrid.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            WeatherInfoGrid.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            WeatherInfoGrid.Size = new Size(446, 100);
+            WeatherInfoGrid.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 900000;
+            timer1.Tick += WeatherTimerTick;
             // 
             // TemperatureValueLabel
             // 
             TemperatureValueLabel.AutoSize = true;
             TemperatureValueLabel.Dock = DockStyle.Fill;
-            TemperatureValueLabel.Location = new Point(3, 0);
+            TemperatureValueLabel.Location = new Point(3, 50);
             TemperatureValueLabel.Name = "TemperatureValueLabel";
-            TemperatureValueLabel.Size = new Size(60, 100);
+            TemperatureValueLabel.Size = new Size(142, 50);
             TemperatureValueLabel.TabIndex = 0;
             TemperatureValueLabel.Text = "...";
             TemperatureValueLabel.TextAlign = ContentAlignment.MiddleCenter;
@@ -52,9 +81,9 @@
             // 
             HumidityValueLabel.AutoSize = true;
             HumidityValueLabel.Dock = DockStyle.Fill;
-            HumidityValueLabel.Location = new Point(69, 0);
+            HumidityValueLabel.Location = new Point(151, 50);
             HumidityValueLabel.Name = "HumidityValueLabel";
-            HumidityValueLabel.Size = new Size(60, 100);
+            HumidityValueLabel.Size = new Size(142, 50);
             HumidityValueLabel.TabIndex = 1;
             HumidityValueLabel.Text = "...";
             HumidityValueLabel.TextAlign = ContentAlignment.MiddleCenter;
@@ -63,55 +92,67 @@
             // 
             RainValueLabel.AutoSize = true;
             RainValueLabel.Dock = DockStyle.Fill;
-            RainValueLabel.Location = new Point(135, 0);
+            RainValueLabel.Location = new Point(299, 50);
             RainValueLabel.Name = "RainValueLabel";
-            RainValueLabel.Size = new Size(62, 100);
+            RainValueLabel.Size = new Size(144, 50);
             RainValueLabel.TabIndex = 2;
             RainValueLabel.Text = "...";
             RainValueLabel.TextAlign = ContentAlignment.MiddleCenter;
             // 
-            // tableLayoutPanel1
+            // TemperatureLabel
             // 
-            tableLayoutPanel1.ColumnCount = 3;
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
-            tableLayoutPanel1.Controls.Add(TemperatureValueLabel, 0, 0);
-            tableLayoutPanel1.Controls.Add(RainValueLabel, 2, 0);
-            tableLayoutPanel1.Controls.Add(HumidityValueLabel, 1, 0);
-            tableLayoutPanel1.Location = new Point(251, 138);
-            tableLayoutPanel1.Name = "tableLayoutPanel1";
-            tableLayoutPanel1.RowCount = 1;
-            tableLayoutPanel1.RowStyles.Add(new RowStyle());
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel1.Size = new Size(200, 100);
-            tableLayoutPanel1.TabIndex = 3;
+            TemperatureLabel.AutoSize = true;
+            TemperatureLabel.Dock = DockStyle.Fill;
+            TemperatureLabel.Location = new Point(3, 0);
+            TemperatureLabel.Name = "TemperatureLabel";
+            TemperatureLabel.Size = new Size(142, 50);
+            TemperatureLabel.TabIndex = 3;
+            TemperatureLabel.Text = "Temperatura";
+            TemperatureLabel.TextAlign = ContentAlignment.MiddleCenter;
             // 
-            // WeatherTimer
+            // HumidityLabel
             // 
-            WeatherTimer.Enabled = true;
-            WeatherTimer.Interval = 900000;
-            WeatherTimer.Tick += WeatherTimerTick;
+            HumidityLabel.AutoSize = true;
+            HumidityLabel.Dock = DockStyle.Fill;
+            HumidityLabel.Location = new Point(151, 0);
+            HumidityLabel.Name = "HumidityLabel";
+            HumidityLabel.Size = new Size(142, 50);
+            HumidityLabel.TabIndex = 4;
+            HumidityLabel.Text = "Wilgotność";
+            HumidityLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // RainLabel
+            // 
+            RainLabel.AutoSize = true;
+            RainLabel.Dock = DockStyle.Fill;
+            RainLabel.Location = new Point(299, 0);
+            RainLabel.Name = "RainLabel";
+            RainLabel.Size = new Size(144, 50);
+            RainLabel.TabIndex = 5;
+            RainLabel.Text = "Opady";
+            RainLabel.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(tableLayoutPanel1);
+            Controls.Add(WeatherInfoGrid);
             Name = "Form1";
             Text = "Form1";
-            tableLayoutPanel1.ResumeLayout(false);
-            tableLayoutPanel1.PerformLayout();
+            WeatherInfoGrid.ResumeLayout(false);
+            WeatherInfoGrid.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
-
+        private TableLayoutPanel WeatherInfoGrid;
+        private System.Windows.Forms.Timer timer1;
         private Label TemperatureValueLabel;
         private Label HumidityValueLabel;
         private Label RainValueLabel;
-        private TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Timer WeatherTimer;
+        private Label TemperatureLabel;
+        private Label HumidityLabel;
+        private Label RainLabel;
     }
 }
--- Pogoda/obj/Debug/net8.0-windows/Pogoda.AssemblyInfo.cs
+++ Pogoda/obj/Debug/net8.0-windows/Pogoda.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pogoda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d58eba9d439e04ec97affb268cb2e7089d567e62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc8205426b14186038b82adc907774dbe9347b1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pogoda")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pogoda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
