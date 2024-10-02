7b112f48c351669b8dd40617fd4139ac61d20e19
Jan Tadeusz Żukowski <4886712@teb-gdansk.ad>
2024-10-02 09:35:23 +0200

    update: added WeatherInfoRaw class

--- Pogoda/Form1.cs
+++ Pogoda/Form1.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using System.Text.Json;
 
 namespace Pogoda
@@ -8,5 +9,42 @@ namespace Pogoda
         {
             InitializeComponent();
         }
+    }
+
+    class WeatherInfoRaw
+    {
+        public WeatherInfoRawUnits current_units;
+        public WeatherInfoRawData current;
+
+
+        public static WeatherInfoRaw Fetch()
+        {
+            HttpClient client = new HttpClient();
+            var res = client.GetAsync("https://api.open-meteo.com/v1/forecast?latitude=54.36&longitude=18.64&current=temperature_2m,relative_humidity_2m,rain").Result;
+            string resContent = res.Content.ReadAsStringAsync().Result;
+
+            WeatherInfoRaw raw = JsonSerializer.Deserialize<WeatherInfoRaw>(resContent);
+
+            if(raw == null)
+            {
+                throw new Exception("Failed to fetch.");
+            }
+            
+            return raw;
+        }
+    }
+
+    class WeatherInfoRawUnits
+    {
+        public string temperature_2m { get; set; }
+        public string relative_humidity_2m { get; set; }
+        public string rain { get; set; }
+    }
+
+    class WeatherInfoRawData
+    {
+        public float temperature_2m { get; set; }
+        public float relative_humidity_2m { get; set; }
+        public float rain { get; set; }
     }
 }
